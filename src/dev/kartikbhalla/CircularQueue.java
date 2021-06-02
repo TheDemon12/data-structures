7f7a50d1e43c0d76acd27491213c67d55841dd81
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-06-03 01:32:50 +0530

    src: part1: implement CircularQueue using Queue
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

---
+++ src/dev/kartikbhalla/CircularQueue.java
@@ -0,0 +1,54 @@
+package dev.kartikbhalla;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CircularQueue extends Queue{
+
+    CircularQueue(int size) {
+        super(size);
+    }
+
+    @Override
+    public void enqueue(int element) {
+        if(isEmpty())
+            front = rear = 0;
+        else if (isFull()) throw new IllegalStateException();
+        else rear = (rear + 1) % array.length;
+
+        array[rear] = element;
+    }
+
+    @Override
+    public int dequeue() {
+        if(isEmpty()) throw new IllegalStateException();
+
+        else if (front == rear) {
+            int element = array[front];
+            front = rear = -1;
+            return element;
+        }
+
+        var element = array[front];
+        front = (front + 1) % array.length;
+        return element;
+    }
+
+    @Override
+    public boolean isFull() {
+        return front == (rear + 1) % array.length;
+    }
+
+    @Override
+    public String toString() {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = front;; i = (i + 1) % array.length) {
+            list.add(array[i]);
+            if (i == rear) break;
+
+        }
+
+        return Arrays.toString(list.toArray());
+    }
+}
--- src/dev/kartikbhalla/Main.java
+++ src/dev/kartikbhalla/Main.java
@@ -14,20 +14,31 @@ public class Main {
 //        Utils.reverseQueue(queue);
 //        System.out.println(queue);
 
-        Queue queue = new Queue(5);
+//        Queue queue = new Queue(5);
+//        queue.enqueue(1);
+//        queue.enqueue(2);
+//        queue.enqueue(3);
+//        queue.enqueue(4);
+//        queue.enqueue(5);
+//
+//        queue.dequeue();
+//        queue.dequeue();
+//        queue.dequeue();
+//        queue.dequeue();
+//        System.out.println(queue);
+//
+//        queue.enqueue(6);
+//        System.out.println(queue);
+
+        CircularQueue queue = new CircularQueue(5);
         queue.enqueue(1);
         queue.enqueue(2);
         queue.enqueue(3);
         queue.enqueue(4);
         queue.enqueue(5);
-
         queue.dequeue();
-        queue.dequeue();
-        queue.dequeue();
-        queue.dequeue();
-        System.out.println(queue);
-
         queue.enqueue(6);
+//        queue.enqueue(7);
         System.out.println(queue);
 
 
